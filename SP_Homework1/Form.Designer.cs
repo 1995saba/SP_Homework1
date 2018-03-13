1e9053f8f9f24a83cf20f6d0f6562a4cab9cf167
Sabina <sabina.orunbaeva@gmail.com>
2018-03-13 13:29:09 +0600

    Add files via upload

---
+++ SP_Homework1/Form.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SP_Homework1
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView = new System.Windows.Forms.ListView();
+            this.button = new System.Windows.Forms.Button();
+            this.processNameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.processIdColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.handleCountColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.processNameColumn,
+            this.processIdColumn,
+            this.handleCountColumn});
+            this.listView.Location = new System.Drawing.Point(12, 12);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(497, 274);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(215, 316);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(75, 23);
+            this.button.TabIndex = 1;
+            this.button.Text = "Processes";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // processNameColumn
+            // 
+            this.processNameColumn.Text = "Process Name";
+            this.processNameColumn.Width = 300;
+            // 
+            // processIdColumn
+            // 
+            this.processIdColumn.Text = "Process ID";
+            this.processIdColumn.Width = 100;
+            // 
+            // handleCountColumn
+            // 
+            this.handleCountColumn.Text = "Handle Count";
+            this.handleCountColumn.Width = 100;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 371);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.listView);
+            this.Name = "Form";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.Button button;
+        public System.Windows.Forms.ColumnHeader processNameColumn;
+        public System.Windows.Forms.ColumnHeader processIdColumn;
+        public System.Windows.Forms.ColumnHeader handleCountColumn;
+    }
+}
+
---
+++ SP_Homework1/Form.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SP_Homework1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+            listView.Enabled = false;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Process[] procList = Process.GetProcesses();
+            listView.Enabled = true;
+            foreach (Process proc in procList)
+            {
+                listView.Items.Add(
+                    new ListViewItem(
+                        new string[] {
+                            $"{proc.ProcessName}", $"{proc.Id}", $"{proc.HandleCount}"
+                        }));
+            }
+        }
+    }
+}
